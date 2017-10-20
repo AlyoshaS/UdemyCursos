62876b17b36fd3d088931409974eabc335ee3590
AlyoshaS <evelynschmitz1@gmail.com>
2017-10-20 16:20:47 -0200

    Alterações no projeto até a aula 51

--- CursoCompletodoDesenvolvedorWeb/Projeto-01/js/efeitos.js
+++ CursoCompletodoDesenvolvedorWeb/Projeto-01/js/efeitos.js
@@ -18,11 +18,34 @@ $(document).ready(function(){
         $("li.search").removeClass("ativo");
 
     });
-
-    $(".thumbnails").owlCarousel({
-
-        //autoplay: 3000,
-        items : 4
-
+    //para o carousel ficar responsivo, é necessário adicionar  quantidade de itens para cada tamanho.
+    $('.thumbnails').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+})
+    // script para as setas de prev e next funcionarem, já com o código atualizado.
+    var owl = $(".thumbnails");
+    owl.owlCarousel();
+    $('#btn-news-prev').on("click", function(){
+      owl.trigger('prev.owl.carousel');
+    });
+    $('#btn-news-next').on("click", function(){
+      owl.trigger('next.owl.carousel');
     });
 });
